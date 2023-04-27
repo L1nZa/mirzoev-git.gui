6d7609c27718de244b1dd9076c60ccbd51e8d7b0
Mirzoev Dovar <l1nza.vac@gmail.com>
2023-04-27 15:31:31 +0300

    Revert "lastname."
    This reverts commit f2be8df2bde1bfbaa814e57c93fed80092a25470.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
-#include <string.h>
+
+#include "askname.h"
 
 int main(int argc, char **argv)
 {
+}    char name[255];
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+    askname(first, last);
+
+    printf("Hello, %s %s!\n", first, last);
+    return 0;
+}
+=======
     printf("Enter your name: ");
     fgets(name, 255, stdin);
+    printf("length = %d\n", strlen(name)); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
     printf("Hello %s!\n", name);
     return 0;
 }
+>>>>>>> parent of f2be8df (lastname.)
+=======
+    printf("Enter your name: ");
+    fgets(name, 255, stdin);
+    printf("length = %d\n", strlen(name)); /* debug line */
+    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+    printf("Hello %s!\n", name);
+    return 0;
+}
+>>>>>>> parent of f2be8df (lastname.)
